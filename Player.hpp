64b31d3187e743a0362f6bbed3f00012f8fcecd3
JE <129920382+sneu13@users.noreply.github.com>
2023-05-08 18:24:16 -0700

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -3,11 +3,31 @@ class Player
 {
 	sf::Texture texture;
 	sf::Sprite sprite;
-	unsigned short x;
+	sf::Clock clock;
+
+	
+	unsigned short x = (0.5f * (320 - 16));
+	unsigned short y;
+	float dx;
+	float dy;
+	const unsigned char x_SPEED = 2.6;
+	float thrustUnit = 1.0;
+	float velocity= 200;
+	sf::Vector2f movement;
+	sf::Vector2f direccion;
+	float newVX, newVY = 0.0;
+	float velx, vely;
+	float det;
+	double _angle;
+	
 
 public:
 	Player();
 	void draw(sf::RenderWindow& i_window);
 	void update();
-};
+	void dt();
+	double Degtorad(double r);
+	void movimiento();
+	void applyThrust();
+	void update
 
