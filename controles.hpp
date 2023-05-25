2e100ed20d424974edd93dca8a097592da9011e0
GikAvecLeStik <131216730+GikAvecLeStik@users.noreply.github.com>
2023-05-25 16:17:42 -0700

    Update controles.hpp

--- controles.hpp
+++ controles.hpp
@@ -9,34 +9,10 @@ class Animation
    public:
    float Frame, speed;
    Sprite sprite;
-   std::vector<IntRect> frames;
+   Animation (Texture &t, int x, int y, int w, int h, int count, float Speed);
+   void update();
+   bool isEnd();
 
-   Animation(){}
-
-   Animation (Texture &t, int x, int y, int w, int h, int count, float Speed)
-   {
-     Frame = 0;
-     speed = Speed;
-
-     for (int i=0;i<count;i++)
-      frames.push_back( IntRect(x+i*w, y, w, h)  );
-
-     sprite.setTexture(t);
-     sprite.setOrigin(w/2,h/2);
-     sprite.setTextureRect(frames[0]);
-   }
-   void update()
-   {
-     Frame += speed;
-     int n = frames.size();
-     if (Frame >= n) Frame -= n;
-     if (n>0) sprite.setTextureRect( frames[int(Frame)] );
-   }
-
-   bool isEnd()
-   {
-     return Frame+speed>=frames.size();
-   }
 
 };
 //--------------------------------------------------------------------------------------------------------------
@@ -49,34 +25,14 @@ class Entity
    std::string name;
    Animation anim;
 
-   Entity()
-   {
-     life=1;
-   }
-
-   void settings(Animation &a,int X,int Y,float Angle=0,int radius=1)
-   {
-     anim = a;
-     x=X; y=Y;
-     angle = Angle;
-     R = radius;
-   }
-
+   Entity();
+   
+   void settings(Animation &a,int X,int Y,float Angle=0,int radius=1);
+ 
    virtual void update(){};
 
-   void draw(RenderWindow &app)
-   {
-     anim.sprite.setPosition(x,y);
-     anim.sprite.setRotation(angle+90);
-     app.draw(anim.sprite);
-
-     CircleShape circle(R);
-     circle.setFillColor(Color(255,0,0,170));
-     circle.setPosition(x,y);
-     circle.setOrigin(R,R);
-     //app.draw(circle);
-   }
-
+   void draw(RenderWindow &app);
+ 
    virtual ~Entity(){};
 };
 //--------------------------------------------------------------------------------------------------------------
@@ -84,22 +40,11 @@ class Entity
 class asteroid: public Entity
 {
    public:
-   asteroid()
-   {
-     dx=rand()%8-4;
-     dy=rand()%8-4;
-     name="asteroid";
-   }
+   asteroid();
+  
 
-   void update()
-   {
-     x+=dx;
-     y+=dy;
-
-     if (x>W) x=0;  if (x<0) x=W;
-     if (y>H) y=0;  if (y<0) y=H;
-   }
-
+   void update();
+  
 };
 //--------------------------------------------------------------------------------------------------------------
 class player: public Entity
@@ -107,32 +52,9 @@ class player: public Entity
    public:
    bool thrust;
 
-   player()
-   {
-     name="player";
-   }
+   player();
 
-   void update()
-   {
-     if (thrust)
-      { dx+=cos(angle*DEGTORAD)*0.1;
-        dy+=sin(angle*DEGTORAD)*0.1; }
-     else
-      { dx*=0.99;
-        dy*=0.99; }
-
-    int maxSpeed = 5;
-    float speed = sqrt(dx*dx+dy*dy);
-    if (speed>maxSpeed)
-     { dx *= maxSpeed/speed;
-       dy *= maxSpeed/speed; }
-
-    x+=dx;
-    y+=dy;
-
-    if (x>W) x=0; if (x<0) x=W;
-    if (y>H) y=0; if (y<0) y=H;
-   }
+   void update();
 
 };
 //--------------------------------------------------------------------------------------------------------------
@@ -140,28 +62,14 @@ class player: public Entity
 class bullet: public Entity
 {
    public:
-   bullet()
-   {
-     name="bullet";
-   }
+   bullet();
+  
 
-   void  update()
-   {
-     dx=cos(angle*DEGTORAD)*6;
-     dy=sin(angle*DEGTORAD)*6;
-     // angle+=rand()%7-3;  /*try this*/
-     x+=dx;
-     y+=dy;
-
-     if (x>W || x<0 || y>H || y<0) life=0;
-   }
+   void  update();
+   
 
 };
 //--------------------------------------------------------------------------------------------------------------
-bool isCollide(Entity *a,Entity *b)
-{
-  return (b->x - a->x)*(b->x - a->x)+
-         (b->y - a->y)*(b->y - a->y)<
-         (a->R + b->R)*(a->R + b->R);
-}
+bool isCollide(Entity *a,Entity *b);
+
 //--------------------------------------------------------------------------------------------------------------
