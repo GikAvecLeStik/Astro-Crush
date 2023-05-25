8a0c75729c531da64108a7392620d058a9980fbc
GikAvecLeStik <131216730+GikAvecLeStik@users.noreply.github.com>
2023-05-25 16:19:52 -0700

    Update controles.hpp

--- controles.hpp
+++ controles.hpp
@@ -9,12 +9,16 @@ class Animation
    public:
    float Frame, speed;
    Sprite sprite;
+   std::vector<IntRect> frames;
+   Animation(){}
    Animation (Texture &t, int x, int y, int w, int h, int count, float Speed);
    void update();
    bool isEnd();
-
+  
 
 };
+
+
 //--------------------------------------------------------------------------------------------------------------
 
 class Entity
