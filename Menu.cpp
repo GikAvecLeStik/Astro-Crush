14715fff5267f6d25ac78d49041d82ed9bcf6657
GikAvecLeStik <131216730+GikAvecLeStik@users.noreply.github.com>
2023-05-29 15:08:04 -0700

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -11,27 +11,28 @@ Menu::Menu(float wid, float hei)
 	}
 	mainmenu[0].setFont(font);
 	mainmenu[0].setFillColor(sf::Color::Black);
-	mainmenu[0].setString("Jugar");
+	mainmenu[0].setString("PLAY");
 	mainmenu[0].setCharacterSize(40);
-	mainmenu[0].setPosition(480, 300);
+	mainmenu[0].setPosition(570,340);
 
 	mainmenu[1].setFont(font);
 	mainmenu[1].setFillColor(sf::Color::Black);
-	mainmenu[1].setString("Instrucciones");
+	mainmenu[1].setString("CONTROLS");
 	mainmenu[1].setCharacterSize(40);
-	mainmenu[1].setPosition(400, 350);
+	mainmenu[1].setPosition(510,430);
 
 	mainmenu[2].setFont(font);
 	mainmenu[2].setFillColor(sf::Color::Black);
-	mainmenu[2].setString("Creditos");
+	mainmenu[2].setString("CREDITS");
 	mainmenu[2].setCharacterSize(40);
-	mainmenu[2].setPosition(360, 390);
+	mainmenu[2].setPosition(535,525);
 
 	mainmenu[3].setFont(font);
 	mainmenu[3].setFillColor(sf::Color::Black);
-	mainmenu[3].setString("Salir");
+	mainmenu[3].setString("EXIT");
 	mainmenu[3].setCharacterSize(40);
-	mainmenu[3].setPosition(640, 420);
+	mainmenu[3].setPosition(580, 615);
+
 
 	menuSelect = -1;
 }
