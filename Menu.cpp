2b455326f372bb0249a660587884ce1788cbc905
JE <129920382+sneu13@users.noreply.github.com>
2023-05-30 22:19:53 -0700

    proMenu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -2,77 +2,251 @@
 #include <iostream>
 #include <cstdlib>
 #include "Menu.hpp"
+#include "Player.hpp"
 
-Menu::Menu(float wid, float hei)
+Player player;
+Menu::Menu()
+    : m_window(sf::VideoMode(1280, 720), "SFML Menu Example")
+    , m_currentState(Option::MainMenu)
 {
-	if (!font.loadFromFile("Fonts/Pixelated Regular.ttf"))
-	{
-		std::cout << "no hay ninguna fuente";
-	}
-	mainmenu[0].setFont(font);
-	mainmenu[0].setFillColor(sf::Color::Black);
-	mainmenu[0].setString("PLAY");
-	mainmenu[0].setCharacterSize(40);
-	mainmenu[0].setPosition(570,340);
+    m_window.setFramerateLimit(60);
 
-	mainmenu[1].setFont(font);
-	mainmenu[1].setFillColor(sf::Color::Black);
-	mainmenu[1].setString("CONTROLS");
-	mainmenu[1].setCharacterSize(40);
-	mainmenu[1].setPosition(510,430);
+    if (!m_backgroundTexture.loadFromFile("Textures/menu.png"))
+    {
+        std::cout << "Error al cargar la imagen de fondo." << std::endl;
+        return;
+    }
+    m_backgroundSprite.setTexture(m_backgroundTexture);
+    m_backgroundSprite.setScale(m_window.getSize().x / static_cast<float>(m_backgroundTexture.getSize().x),m_window.getSize().y / static_cast<float>(m_backgroundTexture.getSize().y));
 
-	mainmenu[2].setFont(font);
-	mainmenu[2].setFillColor(sf::Color::Black);
-	mainmenu[2].setString("CREDITS");
-	mainmenu[2].setCharacterSize(40);
-	mainmenu[2].setPosition(535,525);
+    if (!m_background2Texture.loadFromFile("Textures/espacio.png"))
+    {
+        std::cout << "Error al cargar la imagen de fondo." << std::endl;
+        return;
+    }
+    m_background2Sprite.setTexture(m_background2Texture);
+    m_background2Sprite.setScale(
+        m_window.getSize().x / static_cast<float>(m_background2Texture.getSize().x),
+        m_window.getSize().y / static_cast<float>(m_background2Texture.getSize().y));
 
-	mainmenu[3].setFont(font);
-	mainmenu[3].setFillColor(sf::Color::Black);
-	mainmenu[3].setString("EXIT");
-	mainmenu[3].setCharacterSize(40);
-	mainmenu[3].setPosition(580, 615);
+    if (!m_instructionsTexture.loadFromFile("Textures/menu2.png"))
+    {
+        std::cout << "Error al cargar la imagen de instrucciones." << std::endl;
+        return;
+    }
+    m_instructionsSprite.setTexture(m_instructionsTexture);
 
+    if (!m_creditsTexture.loadFromFile("Textures/menu3.png"))
+    {
+        std::cout << "Error al cargar la imagen de créditos." << std::endl;
+        return;
+    }
+    m_creditsSprite.setTexture(m_creditsTexture);
 
-	menuSelect = -1;
+    if (!m_font.loadFromFile("Fonts/Pixelated Regular.ttf"))
+    {
+        std::cout << "Error al cargar la fuente." << std::endl;
+        return;
+    }
 }
-Menu::~Menu()
-{
 
+void Menu::run()
+{
+    while (m_window.isOpen())
+    {
+        processEvents();
+        update();
+        render();
+    }
 }
 
-void Menu::draw(sf::RenderWindow& window)
+void Menu::processEvents()
 {
-	for (int i = 0; i < maxMenu; ++i)
-	{
+    sf::Event event;
+    while (m_window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            m_window.close();
+        }
 
-		window.draw(mainmenu[i]);
-	}
+        if (event.type == sf::Event::KeyPressed)
+        {
+            if (event.key.code == sf::Keyboard::Escape)
+            {
+                if (m_currentState == Option::MainMenu)
+                {
+                    m_window.close();
+                }
+                else
+                {
+                    m_currentState = Option::MainMenu;
+                }
+            }
+        }
+
+        if (event.type == sf::Event::MouseButtonPressed)
+        {
+            if (event.mouseButton.button == sf::Mouse::Left)
+            {
+                int x = event.mouseButton.x;
+                int y = event.mouseButton.y;
+
+                switch (m_currentState)
+                {
+                case Option::MainMenu:
+                    handleMainMenuClick(x, y);
+                    break;
+                case Option::Instructions:
+                    handleReturnClick(x, y);
+                    break;
+                case Option::Credits:
+                    handleReturnClick(x, y);
+                    break;
+                }
+            }
+        }
+    }
 }
 
-void Menu::moveUp()
+void Menu::update()
 {
-	if (menuSelect - 1 >= 0)
-	{
-		mainmenu[menuSelect].setFillColor(sf::Color::Black);
-		menuSelect--;
-		if (menuSelect == -1)
-		{
-			menuSelect = 2;
-		}
-		mainmenu[menuSelect].setFillColor(sf::Color::Yellow);
-	}
+    // No se requiere lógica de actualización para este ejemplo
 }
-void Menu::moveDown()
+
+void Menu::render()
 {
-	if (menuSelect + 1 <= maxMenu)
-	{
-		mainmenu[menuSelect].setFillColor(sf::Color::Black);
-		menuSelect++;
-		if (menuSelect == 4)
-		{
-			menuSelect = 0;
-		}
-		mainmenu[menuSelect].setFillColor(sf::Color::Yellow);
-	}
+    m_window.clear();
+
+    switch (m_currentState)
+    {
+    case Option::MainMenu:
+        m_window.draw(m_backgroundSprite);
+
+        // Dibujar las opciones del menú principal
+        {
+            sf::Text playText("Jugar", m_font, 30);
+            playText.setPosition(100, 200);
+            m_window.draw(playText);
+        }
+
+        {
+            sf::Text instructionsText("Instrucciones", m_font, 30);
+            instructionsText.setPosition(100, 300);
+            m_window.draw(instructionsText);
+        }
+
+        {
+            sf::Text creditsText("Creditos", m_font, 30);
+            creditsText.setPosition(100, 400);
+            m_window.draw(creditsText);
+        }
+
+        {
+            sf::Text exitText("Salir", m_font, 30);
+            exitText.setPosition(100, 500);
+            m_window.draw(exitText);
+        }
+        break;
+    case Option::Instructions:
+        m_window.draw(m_instructionsSprite);
+
+        // Dibujar el botón de regresar
+        {
+            sf::Text returnText("Regresar", m_font, 20);
+            returnText.setPosition(600, 500);
+            m_window.draw(returnText);
+        }
+        break;
+
+    case Option::Credits:
+        m_window.draw(m_creditsSprite);
+
+        // Dibujar el botón de regresar
+        {
+            sf::Text returnText("Regresar", m_font, 20);
+            returnText.setPosition(600, 500);
+            m_window.draw(returnText);
+        }
+        break;
+    }
+
+    m_window.display();
+}
+
+void Menu::showGame()
+{
+    
+
+    Player player;
+
+    while (m_window.isOpen())
+    {
+        sf::Event event;
+        while (m_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                m_window.close();
+                return;
+            }
+            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+            {
+                return; // Volver al menú principal
+            }
+        }
+        player.dt();
+        player.update();
+
+        m_window.clear();
+        m_window.draw(m_background2Sprite);
+        player.draw(m_window);
+        
+        m_window.display();
+    }
+}
+
+void Menu::handleMainMenuClick(int x, int y)
+{
+    if (x >= 100 && x <= 300 && y >= 200 && y <= 250)
+    {
+        std::cout << "Iniciar juego" << std::endl;
+        // Aquí puedes agregar el código para iniciar el juego
+
+        showGame();
+    }
+    else if (x >= 100 && x <= 300 && y >= 300 && y <= 350)
+    {
+        std::cout << "Mostrar instrucciones" << std::endl;
+        m_currentState = Option::Instructions;
+    }
+    else if (x >= 100 && x <= 300 && y >= 400 && y <= 450)
+    {
+        std::cout << "Mostrar créditos" << std::endl;
+        m_currentState = Option::Credits;
+    }
+    else if (x >= 100 && x <= 300 && y >= 500 && y <= 550)
+    {
+        std::cout << "Salir del juego" << std::endl;
+        m_window.close();
+    }
+}
+
+void Menu::handleInstructionsClick(int x, int y)
+{
+    // No se requiere lógica específica para este ejemplo
+}
+
+void Menu::handleCreditsClick(int x, int y)
+{
+    // No se requiere lógica específica para este ejemplo
+}
+
+void Menu::handleReturnClick(int x, int y)
+{
+    if (x >= 600 && x <= 700 && y >= 500 && y <= 550)
+    {
+        std::cout << "Regresar al menú principal" << std::endl;
+        m_currentState = Option::MainMenu;
+    }
 }
